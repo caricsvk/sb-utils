15534b35a762891a423e393951f68aed1c9cb4fc
Richard Casar <caricsvk@gmail.com>
2021-02-23 18:49:09 +0100

    simplify datetimehelper

--- src/main/java/milo/utils/DateTimeHelper.java
+++ src/main/java/milo/utils/DateTimeHelper.java
@@ -10,15 +10,13 @@ public class DateTimeHelper {
 		if (timestamp == null) {
 			return null;
 		}
-		return LocalDateTime.ofEpochSecond(timestamp.getTime() / 1000, timestamp.getNanos(), ZoneOffset.UTC);
+		return timestamp.toLocalDateTime();
 	}
 
 	public static Timestamp toTimestamp(LocalDateTime localDateTime) {
 		if (localDateTime == null) {
 			return null;
 		}
-		Timestamp timestamp = new Timestamp(localDateTime.toEpochSecond(ZoneOffset.UTC) * 1000);
-		timestamp.setNanos(localDateTime.getNano());
-		return timestamp;
+		return Timestamp.valueOf(localDateTime);
 	}
 }
