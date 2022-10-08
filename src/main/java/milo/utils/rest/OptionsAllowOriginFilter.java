9759362eb1a6d4af4f7b3057e892201a81ad7777
Richard Casar <caricsvk@gmail.com>
2022-10-07 19:30:11 -0700

    make CustomHeaderFilter and OptionsAllowOriginFilter to support credentials

--- src/main/java/milo/utils/rest/OptionsAllowOriginFilter.java
+++ src/main/java/milo/utils/rest/OptionsAllowOriginFilter.java
@@ -5,16 +5,30 @@ import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Provider
 public class OptionsAllowOriginFilter implements ContainerResponseFilter {
 
+	public static List<String> credentialsOriginsPrefixes = new ArrayList();
+
 	@Override
 	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
 		if ("OPTIONS".equals(requestContext.getMethod())) {
-			responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", "*");
-			responseContext.getHeaders().putSingle("Access-Control-Allow-Headers", "*");
-//			responseContext.getHeaders().add("Allow", String.join(", ", responseContext.getAllowedMethods()));
+			String requestedOrigin = requestContext.getHeaderString("origin");
+			if (requestedOrigin == null) {
+				requestedOrigin = "";
+			}
+			boolean allowedOrigin = credentialsOriginsPrefixes.stream().anyMatch(requestedOrigin::startsWith);
+			responseContext.getHeaders().putSingle("Access-Control-Allow-Credentials", "true");
+			responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", allowedOrigin ? requestedOrigin : "*");
+			responseContext.getHeaders().add("Access-Control-Allow-Methods",
+					String.join(", ", responseContext.getAllowedMethods())
+			);
+			responseContext.getHeaders().putSingle("Access-Control-Allow-Headers",
+					"Content-Type, Accept, Cookie, prevent-default-error-handling"
+			);
 		}
 	}
 
