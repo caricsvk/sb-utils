7f9e40876ada72f20b6b3b4066060c1ea792f5ad
Richard Casar <caricsvk@gmail.com>
2023-03-20 13:10:14 -0700

    fix "Failed to generate the schema for the JAX-B elements 1 counts of IllegalAnnotationExceptions" Exception

--- src/main/java/milo/utils/rest/OptionsAllowOriginFilter.java
+++ src/main/java/milo/utils/rest/OptionsAllowOriginFilter.java
@@ -1,36 +1,35 @@
 package milo.utils.rest;
 
 import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.container.ContainerResponseContext;
-import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-@Provider
-public class OptionsAllowOriginFilter implements ContainerResponseFilter {
+@Provider @PreMatching
+public class OptionsAllowOriginFilter implements ContainerRequestFilter {
 
 	public static List<String> credentialsOriginsPrefixes = new ArrayList();
 
 	@Override
-	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+	public void filter(ContainerRequestContext requestContext) throws IOException {
 		if ("OPTIONS".equals(requestContext.getMethod())) {
 			String requestedOrigin = requestContext.getHeaderString("origin");
 			if (requestedOrigin == null) {
 				requestedOrigin = "";
 			}
 			boolean allowedOrigin = credentialsOriginsPrefixes.stream().anyMatch(requestedOrigin::startsWith);
-			responseContext.getHeaders().putSingle("Access-Control-Allow-Credentials", "true");
-			responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", allowedOrigin ? requestedOrigin : "*");
-			responseContext.getHeaders().add("Access-Control-Allow-Methods",
-					String.join(", ", responseContext.getAllowedMethods())
-			);
-			responseContext.getHeaders().putSingle("Access-Control-Allow-Headers",
-					"Content-Type, Accept, Cookie," +
-					"prevent-default-error-handling, exclude-from-counting, prevent-default-cancellation"
-			);
+			Response response = Response.ok()
+					.header("Access-Control-Allow-Credentials", "true")
+					.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+					.header("Access-Control-Allow-Origin", allowedOrigin ? requestedOrigin : "*")
+					.header("Access-Control-Allow-Headers", "Content-Type, Accept, Cookie, " +
+							"prevent-default-error-handling, exclude-from-counting, prevent-default-cancellation")
+					.build();
+			requestContext.abortWith(response);
 		}
 	}
-
 }
